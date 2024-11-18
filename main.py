d6aa052c2b8a6054f67070740c2d2c3e58adfbe5
randomflamingguy <randomflamingguy1@gmail.com>
2024-11-18 18:22:09 +0600

    yguguuygutf

---
+++ main.py
@@ -0,0 +1,24 @@
+def add():
+    num1 = int(input("Please enter your first number:"))
+    num2 = int(input("Please enter your second number:"))
+    option = input("What option would you like? 1)addition 2)substraction 3)multiplication 4)division: ")
+    return num1 + num2
+print("addition:", add())
+def sub():
+    num1 = int(input("Please enter your first number:"))
+    num2 = int(input("Please enter your second number:"))
+    option = input("What option would you like? 1)addition 2)substraction 3)multiplication 4)division: ")
+    return num1 - num2
+print("substraction:", sub())
+def mul():
+    num1 = int(input("Please enter your first number:"))
+    num2 = int(input("Please enter your second number:"))
+    option = input("What option would you like? 1)addition 2)substraction 3)multiplication 4)division: ")
+    return num1 * num2
+print("multiplication:", mul())
+def div():
+    num1 = int(input("Please enter your first number:"))
+    num2 = int(input("Please enter your second number:"))
+    option = input("What option would you like? 1)addition 2)substraction 3)multiplication 4)division: ")
+    return num1 / num2
+print("division:", div())
